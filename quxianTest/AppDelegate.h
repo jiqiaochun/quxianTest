6307a3f102bfc1862594be451a6f2e14a6e70ef5
@jiqiaochun <13011251196@163.com>
2017-03-23 15:14:21 +0800

    Initial Commit

---
+++ quxianTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quxianTest
+//
+//  Created by 姬巧春 on 2017/3/23.
+//  Copyright © 2017年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quxianTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  quxianTest
+//
+//  Created by 姬巧春 on 2017/3/23.
+//  Copyright © 2017年 姬巧春. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
